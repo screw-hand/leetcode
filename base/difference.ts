d1053d0a0e464d9955bf8b26a428dc98b66d5da6
screw-hand <screwhand0@gmail.com>
2022-05-19 19:00:55 +0800

    Create difference.ts

---
+++ base/difference.ts
@@ -0,0 +1,21 @@
+/**
+ * LABULADONG 的算法网站
+ * 小而美的算法技巧：差分数组
+ * https://labuladong.gitee.io/algo/2/18/23/
+ */
+class Difference {
+  private diff: number[] = [];
+
+  constructor(nums: number[]) {}
+
+  /**
+   * increment
+   */
+  public increment(i: number, j: number, val: number): void {}
+
+  public result(): number[] {
+    return [];
+  }
+}
+
+export default Difference;
