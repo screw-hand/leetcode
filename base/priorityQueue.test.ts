aef01d41093306aa183c539b5293a5c626ecba11
screw-hand <screwhand0@gmail.com>
2022-05-23 16:12:11 +0800

    MinPQ

--- base/priorityQueue.test.ts
+++ base/priorityQueue.test.ts
@@ -1,4 +1,4 @@
-import { MaxPQ } from "./priorityQueue";
+import { MaxPQ, MinPQ } from "./priorityQueue";
 
 const nums = ["A", "O", "N", "P", "R", "S", "T"];
 
@@ -16,6 +16,27 @@ test(`MaxMQ init [${nums}]`, () => {
   expect(maxMQ.delMax()).toEqual("T");
 
   expect(maxMQ.getPq()).toEqual(["T", "P", "S", "A", "O", "N", "R"]);
+
+  expect(maxMQ.getN()).toEqual(7);
+});
+
+test(`MinPQ init [${nums}]`, () => {
+  const maxMQ = new MinPQ<string>(
+    nums.length,
+    (a, b) => a.charCodeAt(0) > b.charCodeAt(0)
+  );
+  nums.forEach((x) => maxMQ.insert(x));
+  expect(maxMQ.getPq()).toEqual(["A", "O", "N", "P", "R", "S", "T"]);
+  expect(maxMQ.getN()).toEqual(7);
+
+  maxMQ.insert("Z");
+
+  expect(maxMQ.getPq()).toEqual(["A", "O", "N", "P", "R", "S", "T", "Z"]);
+  expect(maxMQ.getN()).toEqual(8);
+
+  expect(maxMQ.delMin()).toEqual("O");
+
+  expect(maxMQ.getPq()).toEqual(["O", "R", "N", "P", "Z", "S", "T"]);
 
   expect(maxMQ.getN()).toEqual(7);
 });
--- base/priorityQueue.ts
+++ base/priorityQueue.ts
@@ -29,7 +29,7 @@ class MaxPQ<T = number> {
     this.pq.pop();
     this.N--;
     this.sink(1);
-    return this.pq[1];
+    return this.max();
   }
 
   /* 上浮第k个元素，以维护最大堆性质 */
@@ -93,4 +93,93 @@ class MaxPQ<T = number> {
   }
 }
 
-export { MaxPQ };
+class MinPQ<T = number> {
+  private pq: Array<T> = [];
+  private N: number = 0;
+  private sort: (a: T, b: T) => boolean;
+
+  constructor(cap: number, sort: (a: T, b: T) => boolean) {
+    this.pq = new Array(cap + 1);
+    this.sort = sort;
+  }
+
+  /* 返回当前队列最小元素 */
+  public min(): T {
+    return this.pq[1];
+  }
+
+  /* 插入元素e */
+  public insert(e: T) {
+    this.N++;
+    this.pq[this.N] = e;
+    this.swim(this.N);
+  }
+
+  /* 删除并返回当前队列中最小元素 */
+  public delMin() {
+    this.exch(1, this.N);
+    this.pq.pop();
+    this.N--;
+    this.sink(1);
+    return this.min();
+  }
+
+  /* 上浮第k个元素 */
+  private swim(k: number) {
+    while (k > 1 && this.moreThen(this.parent(k), k)) {
+      this.exch(this.parent(k), k);
+      k = this.parent(k);
+    }
+  }
+
+  /* 下浮第k个元素 */
+  private sink(k: number) {
+    while (this.left(k) <= this.N) {
+      let older = this.left(k);
+      if (this.moreThen(this.right(k), this.left(k))) {
+        older = this.right(k);
+      }
+      if (this.moreThen(older, k)) {
+        break;
+      }
+      this.exch(older, k);
+      k = older;
+    }
+  }
+
+  /* 交换数组的两个元素 */
+  private exch(i: number, j: number) {
+    const temp = this.pq[i];
+    this.pq[i] = this.pq[j];
+    this.pq[j] = temp;
+  }
+
+  /* 比较索引i的值是否比索引j的值大 */
+  private moreThen(i: number, j: number): boolean {
+    return this.sort(this.pq[i], this.pq[j]);
+  }
+
+  parent(root: number): number {
+    return Math.floor(root / 2);
+  }
+
+  left(root: number): number {
+    return root * 2;
+  }
+
+  right(root: number): number {
+    return root * 2 + 1;
+  }
+
+  getPq() {
+    const pq = [...this.pq];
+    pq.shift();
+    return pq;
+  }
+
+  getN() {
+    return this.N;
+  }
+}
+
+export { MaxPQ, MinPQ };
